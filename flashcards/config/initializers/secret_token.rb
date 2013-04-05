ec23c9c3e6656e4be2439820a2f915d6d7918b8d
Fernando Paredes <nanoxd@me.com>
2013-04-05 08:34:44 -0700

    Modify gemfile

---
+++ flashcards/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flashcards::Application.config.secret_token = 'cca03075f778cabfd6822c30eafb4c71940b94921bf5b7a28a209a0a5a9c71756be1b92961cc7ab4e527debd73f159f4e7add4e467a7ef3454fef1eee9804da6'
