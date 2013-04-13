2d423807895f377554093121bfad2f3ee6734980
Fernando Paredes <nanoxd@me.com>
2013-04-13 09:53:56 -0700

    Complete Card CRUD actions and views.

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -3,30 +3,36 @@ class CardsController < ApplicationController
   def show
     @deck = Deck.find(params[:deck_id])
     @card = @deck.cards.find(params[:id])
+    # Could also be written as:
+    # @card = Card.find(params[:id])
   end
 
   def new
-
+    @deck = Deck.find(params[:deck_id])
+    @card = @deck.cards.new
   end
 
   def create
     @deck = Deck.find(params[:deck_id])
-    @card = Card.new(params[:front, :back])
-    @deck.cards  << @card
+    @card = @deck.cards.new(params[:card])
+    @card.save
+    redirect_to [@deck, @card]
   end
 
   def edit
-    @deck = Deck.find(params[:deck_id])
-    @card = @deck.cards.find(params[:id])
+    @card = Card.find(params[:id])
   end
 
   def update
-    @
-
+    @card = Card.find(params[:id])
+    @card.update_attributes(params[:card])
+    redirect_to [@card.deck, @card]
   end
 
   def destroy
-
+    @card = Card.find(params[:id])
+    @card.destroy
+    redirect_to deck_path params[:deck_id]
   end
 
 end
