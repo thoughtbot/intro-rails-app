b68d42753235143873d94572274b7d792b8b7011
Fernando Paredes <nanoxd@me.com>
2013-04-12 17:27:36 -0700

    Refactor paths based on polymorphic_path's ability to do the guesswork

--- flashcards/config/routes.rb
+++ flashcards/config/routes.rb
@@ -1,9 +1,3 @@
 Flashcards::Application.routes.draw do
-  get "/decks" => "decks#index"
-  get "/decks/new" => "decks#new"
-  get "/decks/:id" => "decks#show", as: :deck
-  post "/decks" => "decks#create"
-  get "/decks/:id/edit" => "decks#edit"
-  put "/decks/:id" => "decks#update"
-  delete "/decks/:id" => "decks#destroy"
+  resources :decks
 end
