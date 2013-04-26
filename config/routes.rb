7ddc9efaeb2e6a675fd42f9f1506ffb572bee7f8
James Frost <james@jameswfrost.co.uk>
2013-04-26 08:17:01 +0100

    Began adding user sessions.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Flashcards::Application.routes.draw do
+  root to: "homes#index"
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create]
   resources :decks do
     resources :cards, except: :index
   end
